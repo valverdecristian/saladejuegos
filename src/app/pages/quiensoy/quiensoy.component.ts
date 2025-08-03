0877158eca95a9011e77026438255fbee9bdcf9d
valverdecristian <crizthian2010@gmail.com>
2025-08-03 12:39:51 -0300

    ADD: estilos

--- src/app/pages/quiensoy/quiensoy.component.ts
+++ src/app/pages/quiensoy/quiensoy.component.ts
@@ -1,25 +1,24 @@
-import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
-import { Component, computed, OnInit, signal } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GithubService } from '../../services/github.service';
 
 @Component({
   selector: 'app-quiensoy',
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './quiensoy.component.html',
   styleUrl: './quiensoy.component.css',
 })
 export class QuiensoyComponent implements OnInit {
-  gitHubUser = signal<any>({});
-  githubEntries = computed(() => Object.entries(this.gitHubUser()));
+  nombre: string = '';
+  avatar: string = '';
+  bio: string = '';
 
-  constructor(private http: HttpClient) {}
+  constructor(private githubService: GithubService) {}
 
   ngOnInit(): void {
-    this.http
-      .get<any>('https://api.github.com/users/valverdecristian')
-      .subscribe({
-        next: (data) => this.gitHubUser.set(data),
-        error: (err) => console.error('Error al obtener usuario', err),
-      });
+    this.githubService.getUserData().subscribe((data: any) => {
+      this.nombre = data.name;
+      this.avatar = data.avatar_url;
+      this.bio = data.bio;
+    })
   }
 }
--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,16 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class GithubService {
+  private apiUrl = 'https://api.github.com/users/valverdecristian';
 
-  private baseUrl = 'https://api.github.com/users/';
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  getUserData(username: string) {
-    return this.http.get(`${this.baseUrl}${username}`);
+  getUserData() {
+    return this.http.get(this.apiUrl);
   }
 }
